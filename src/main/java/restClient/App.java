f07fc08d5575141648a6bbdf2bd2894361ab1e29
Sergey Popov <popovsv1987@gmail.com>
2022-07-26 14:11:06 +0300

    initial commit

--- src/main/java/restClient/App.java
+++ src/main/java/restClient/App.java
@@ -1,23 +1,49 @@
 package restClient;
 
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import restClient.configuration.Config;
-import restClient.model.CommunicatorNEW;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+
 import restClient.model.User;
 
 import java.util.List;
 
-
+@SpringBootApplication
 public class App {
 
+
+    private static RestTemplate restTemplate = new RestTemplate();
+    private static final String URL = "http://94.198.50.185:7081/api/users";
+
     public static void main( String[] args ) {
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(Config.class);
-        CommunicatorNEW communicatorNEW = context.getBean(CommunicatorNEW.class);
 
-        System.out.println(communicatorNEW.getInfo());
+        ResponseEntity<List<User>> responseEntity = restTemplate.exchange(URL, HttpMethod.GET, null,
+                new ParameterizedTypeReference<>() {});
+        HttpHeaders headers = responseEntity.getHeaders();
+        System.out.println(headers.getFirst(HttpHeaders.SET_COOKIE));
 
+        String sessionId = headers.getFirst(HttpHeaders.SET_COOKIE);
+
+
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.add("Cookie", sessionId);
+        requestHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
+        System.out.println(requestHeaders);
+
+        User newUser = new User(3L, "James", "Brown", (byte)12);
+
+
+        HttpEntity<User> requestEntity = new HttpEntity<>(newUser, requestHeaders);
+        System.out.println(requestEntity);
+
+        ResponseEntity<String> response = restTemplate.exchange(URL, HttpMethod.POST, requestEntity, String.class);
+        System.out.println(response.getBody());
     }
+
 
 }
--- src/main/java/restClient/configuration/Config.java
+++ src/main/java/restClient/configuration/Config.java
@@ -1,17 +1,17 @@
-package restClient.configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-
-@Configuration
-@ComponentScan("restClient")
-
-public class Config {
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-}
+//package restClient.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.client.RestTemplate;
+//
+//@Configuration
+//@ComponentScan("restClient")
+//
+//public class Config {
+//
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
+//}
